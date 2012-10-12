606702ed2aa991b20102e66d901f3a85d8e224d2
Ottowan@gmail.com <Ottowan@gmail.com@dd2fac05-a434-c35f-77d5-eb2919b9e563>
2012-10-12 05:11:19 +0000

    Fixed 1

--- trunk/application/models/tour_model.php
+++ trunk/application/models/tour_model.php
@@ -4,111 +4,65 @@ class Tour_model extends MY_Model {
     parent::__construct();
     $this->_prefix = "tou";
     $this->_column = array(
-                     'id'                         => 'tou_id',
-                     'language_id'                => 'tou_language_id',
-                     'code'                       => 'tou_code',
-                     'name'                       => 'tou_name',
-                     'description'                => 'tou_description',
-                     'detail'                     => 'tou_detail',
-                     'included'                   => 'tou_included',
-                     'remark'                     => 'tou_remark',
-                     'discount'                   => 'tou_discount',
-                     'thai_net_adult_price'       => 'tou_thai_net_adult_price',
-                     'thai_net_child_price'       => 'tou_thai_net_child_price',
-                     'thai_sale_adult_price'      => 'tou_thai_sale_adult_price',
-                     'thai_sale_child_price'      => 'tou_thai_sale_child_price',
-                     'foreigner_net_adult_price'  => 'tou_foreigner_net_adult_price',
-                     'foreigner_net_child_price'  => 'tou_foreigner_net_child_price',
-                     'foreigner_sale_adult_price' => 'tou_foreigner_sale_adult_price',
-                     'foreigner_sale_child_price' => 'tou_foreigner_sale_child_price',
-                     'amount_time'                => 'tou_amount_time',
-                     'start_time'                 => 'tou_start_time',
-                     'end_time'                   => 'tou_end_time',
-                     'cr_date'                    => 'tou_cr_date',
-                     'cr_uid'                     => 'tou_cr_uid',
-                     'lu_date'                    => 'tou_lu_date',
-                     'lu_uid'                     => 'tou_lu_uid'  
+                     'id'                => 'tou_id',
+                     'language_id'       => 'tou_language_id',
+                     'code'              => 'tou_code',
+                     'name'              => 'tou_name',
+                     'description'       => 'tou_description',
+                     'detail'            => 'tou_detail',
+                     'included'          => 'tou_included',
+                     'remark'            => 'tou_remark',
+                     'discount'          => 'tou_discount',
+                     'net_adult_price'   => 'tou_net_adult_price',
+                     'net_child_price'   => 'tou_net_child_price',
+                     'sale_adult_price'  => 'tou_sale_adult_price',
+                     'sale_child_price'  => 'tou_sale_child_price',
+                     'amount_time'       => 'tou_amount_time',
+                     'start_time'        => 'tou_start_time',
+                     'end_time'          => 'tou_end_time',
+                     'cr_date'           => 'tou_cr_date',
+                     'cr_uid'            => 'tou_cr_uid',
+                     'lu_date'           => 'tou_lu_date',
+                     'lu_uid'            => 'tou_lu_uid'  
     );
   }
   
+  function mapField($query){
+
+    foreach ($query as $key => $value) {
+      foreach ($value as $keyField => $valueFiled) {
+        //explode field name
+        $keyExplode = explode("_", $keyField, 2);
+        $newkey = $keyExplode[1];
+
+        //Create new data
+        $data->$newkey = $valueFiled;
+      }
+      $newQuery[] = $data;
+    }
+    return $newQuery;
+  }
   
-  function getRecord($data){
-    $query = $this->db->get("ci_tour");
-    return $query->result();
+  function getRecord($id=false){
+
+    if($id){
+      $query = $this->db->get_where('ci_tour', array('tou_id' => $id), 1, 0);
+
+      if($query->num_rows > 0){
+        $newQuery = $this->mapField($query->result());
+        return $newQuery;
+      }else{
+        return false;
+      }
+    }else{
+      $query = $this->db->get("ci_tour");
+      return $query->result();      
+    }
+
   }
 
 
-  function addRecord($data=flase){
-/*
-    //Validate data
-    if($data==""){
-      return FALSE;
-    }
-    if(!$this->_required("name",$data)){
-      $this->_addError("Please enter description");
-      return FALSE;
-    }
-    if(!$this->_required("description",$data)){
-      $this->_addError("Please enter description");
-      return FALSE;
-    }
-    if(!$this->_required("detail",$data)){
-      $this->_addError("Please enter detail");
-      return FALSE;
-    }
-
-    //Validate thai price
-    if(!$this->_required("thai_net_adult_price",$data)){
-      $this->_addError("Please enter net price for adult(th)");
-      return FALSE;
-    }
-    if(!$this->_required("thai_net_child_price",$data)){
-      $this->_addError("Please enter net price for child(th)");
-      return FALSE;
-    }
-    if(!$this->_required("thai_sale_adult_price",$data)){
-      $this->_addError("Please enter sale price for adult(th)");
-      return FALSE;
-    }
-    if(!$this->_required("thai_sale_child_price",$data)){
-      $this->_addError("Please enter sale price for child(th)");
-      return FALSE;
-    }
-
-    //Validate foreigner price
-    if(!$this->_required("foreigner_net_adult_price",$data)){
-      $this->_addError("Please enter net price for adult(foreigner)");
-      return FALSE;
-    }
-    if(!$this->_required("foreigner_net_child_price",$data)){
-      $this->_addError("Please enter net price for child(foreigner)");
-      return FALSE;
-    }
-    if(!$this->_required("foreigner_sale_adult_price",$data)){
-      $this->_addError("Please enter sale price for adult(foreigner)");
-      return FALSE;
-    }
-    if(!$this->_required("foreigner_sale_child_price",$data)){
-      $this->_addError("Please enter sale price for child(foreigner)");
-      return FALSE;
-    }
-
-
-    //Validate time
-    if(!$this->_required("amount_time",$data)){
-      $this->_addError("Please enter net price for child(foreigner)");
-      return FALSE;
-    }
-    if(!$this->_required("start_time",$data)){
-      $this->_addError("Please enter sale price for adult(foreigner)");
-      return FALSE;
-    }
-    if(!$this->_required("end_time",$data)){
-      $this->_addError("Please enter sale price for child(foreigner)");
-      return FALSE;
-    }
-
-*/
+  function addRecord($data=false){
 
     if($data){
       //Set data
@@ -118,17 +72,25 @@ class Tour_model extends MY_Model {
         }
       }
       $result = $this->db->insert($this->_table);
-    }else{
-
-
     }
     
     return ;
   }
   
-  function updateRecord($data){
-    $query = $this->db->where("id", $data["id"]);
-    $query = $this->db->update("ci_tour", $data);
+
+  function updateRecord($data=false){
+    if($data){
+      //Set data
+      foreach($data AS $columnName=>$columnValue){
+        if(array_key_exists($columnName, $this->_column)){
+          $this->db->set($this->_column[$columnName], $columnValue); 
+        }
+      }
+      $query = $this->db->where("tou_id", $data["id"]);
+      $query = $this->db->update("ci_tour");
+    }
+    
+    return ;
   }
 
   function deleteRecord(){
