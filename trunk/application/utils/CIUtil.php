0f6ff87b24795de330f1982e6bde120289bc0f06
Ottowan@gmail.com <Ottowan@gmail.com@dd2fac05-a434-c35f-77d5-eb2919b9e563>
2012-11-02 10:05:20 +0000

    Add new function for gennerate SEO url

--- trunk/application/utils/CIUtil.php
+++ trunk/application/utils/CIUtil.php
@@ -26,6 +26,23 @@ class Util {
     return base_url('themes/'.Config::getConfig("theme_name"));
   }
   
+  public static function url_title($title=false){
+  	if($title){
+		$string = $title;
+		$string = preg_replace("`\[.*\]`U","",$string);
+		$string = preg_replace('`&(amp;)?#?[a-z0-9]+;`i','-',$string);
+		$string = str_replace('%', '-percent', $string);
+		$string = htmlentities($string, ENT_COMPAT, 'utf-8');
+		$string = preg_replace( "`&([a-z])(acute|uml|circ|grave|ring|cedil|slash|tilde|caron|lig|quot|rsquo);`i","\\1", $string );
+		$string = preg_replace( array("`[^a-z0-9ก-๙เ-า]`i","`[-]+`") , "-", $string);
+		$string = strtolower(trim($string, '-'));
+		$string = trim($string);
+		return $string;
+  	}else{
+  		return false;
+  	}
+
+  }
 }
 
 ?>
