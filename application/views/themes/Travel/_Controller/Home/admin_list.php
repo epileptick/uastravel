c92384df7bceb74baffcfbfd84a7b17738a930b3
Ottowan <Ottowan@gmail.com>
2012-11-08 07:45:58 +0000

    Add view admin

---
+++ application/views/themes/Travel/_Controller/Home/admin_list.php
@@ -0,0 +1,13 @@
+
+
+<a href="<?php echo base_url("admin/tag");?>">tag</a><br/><br/>
+
+<a href="<?php echo base_url("admin/tour");?>">tour</a><br/><br/>
+
+<a href="<?php echo base_url("admin/agency");?>">agency</a><br/><br/>
+
+<a href="<?php echo base_url("admin/location");?>">location</a><br/><br/>
+
+
+<a href="<?php echo base_url("admin/type");?>">type</a><br/><br/>
+
