df28736678fb2c9ad6ecdec1324a467f8eb3c678
Ottowan <Ottowan@gmail.com>
2012-11-22 06:54:02 +0000

    Make new query for view page

--- application/models/tagtour_model.php
+++ application/models/tagtour_model.php
@@ -53,7 +53,7 @@ class TagTour_model extends MY_Model {
   function getRecord($args=false){
     //print_r($args); exit;
 
-    if(!empty($args["tag_id"]) && !empty($args["tour_id"]) ){
+    if(!empty($args["tag_id"]) && !empty($args["tour_id"]) && empty($args["join"]) && empty($args["firstpage"])  && empty($args["related"])){
       //Get category by name
 
       $data["tat_tag_id"] = $args["tag_id"];
@@ -76,7 +76,6 @@ class TagTour_model extends MY_Model {
         $this->db->limit($args["per_page"], $args["offset"]);
       }  
       $firsttour = $this->db->get('ci_tagtour')->result(); 
-
       if(!empty($firsttour)){
 
         $count = 0;
@@ -98,7 +97,7 @@ class TagTour_model extends MY_Model {
 
             //Get tour data
             unset($this->db);
-            $this->db->select('tou_id, tou_name, tou_code, tou_url, tou_first_image');
+            $this->db->select('tou_id, tou_name, tou_code, tou_url, tou_first_image, tou_banner_image');
             $this->db->where('tou_id', $value->tat_tour_id);
             $query = $this->db->get('ci_tour');
             $tourBuffer = $query->result(); 
@@ -112,6 +111,23 @@ class TagTour_model extends MY_Model {
             $this->db->join('ci_tag', 'ci_tag.tag_id = ci_tagtour.tat_tag_id');
             $query = $this->db->get('ci_tagtour');
             $result[$count]["tag"] = $query->result();
+
+            //Get price data
+            unset($this->db);
+            $this->db->where('agt_tour_id', $value->tat_tour_id);
+            $priceTour = $this->db->get('ci_agencytour')->result();
+
+            if(!empty($priceTour)){
+              $maxAgencyPrice = 0;
+              foreach ($priceTour as $key => $value) {
+                # code...
+                if($value->agt_sale_adult_price > $maxAgencyPrice){
+                  $result[$count]["price"] = $value;
+                  $maxAgencyPrice = $value->agt_sale_adult_price;
+                }
+              }
+            }
+
 
             $count++;
           }
@@ -128,23 +144,69 @@ class TagTour_model extends MY_Model {
         return false;
       }
 
-    }else if(!empty($args["tag_id"]) && !empty($args["join"]) && !empty($args["in"])){
+    }else if(!empty($args["tag_id"]) && !empty($args["join"]) && !empty($args["in"]) && !empty($args["related"])){
 
-      //$this->db->select('tat_tour_id, tou_name, tag_id, tag_name, tag_url');
-      //$this->db->distinct("tat_tour_id");  
-      /*
-      $this->db->join('ci_tag', 'ci_tag.tag_id = ci_tagtour.tat_tag_id');
-      $this->db->join('ci_tour', 'ci_tour.tou_id = ci_tagtour.tat_tour_id');         
+      //Get distinct tour_id from ci_tagtour
 
+      $this->db->select('tat_tour_id');
+      $this->db->distinct("tat_tour_id");
+      $this->db->where('tat_tour_id  != ', $args["tour_id"]);  
       $this->db->where_in('tat_tag_id', $args["tag_id"]);  
-
-      $this->db->order_by('CONVERT( ci_tour.tou_name USING tis620 ) ASC');  
-
+      $this->db->join('ci_tour', 'ci_tour.tou_id = ci_tagtour.tat_tour_id');         
+      $this->db->order_by('tat_tour_id DESC');  
       if($args["per_page"] > -1 && $args["offset"] > -1){
         $this->db->limit($args["per_page"], $args["offset"]);
       }
-      //print_r($args); exit; 
-      */
+      $tour = $this->db->get('ci_tagtour')->result(); 
+
+
+      $count = 0;
+      foreach ($tour as $key => $value) {
+
+        //Get tour data
+        unset($this->db);
+        $this->db->select('tou_id, tou_name, tou_code, tou_url, tou_first_image, tou_banner_image');
+        $this->db->where('tou_id', $value->tat_tour_id);
+        $query = $this->db->get('ci_tour');
+        $tourBuffer = $query->result(); 
+        $result[$count]["tour"] = $tourBuffer[0];
+
+
+        //Get tag data
+        unset($this->db);
+        $this->db->where('tat_tour_id', $value->tat_tour_id);
+        $this->db->where_in('tat_tag_id', $args["menu"]);
+        $this->db->join('ci_tag', 'ci_tag.tag_id = ci_tagtour.tat_tag_id');
+        $query = $this->db->get('ci_tagtour');
+        $result[$count]["tag"] = $query->result();
+
+
+        //Get price data
+        unset($this->db);
+        $this->db->where('agt_tour_id', $value->tat_tour_id);
+        $priceTour = $this->db->get('ci_agencytour')->result();
+
+        if(!empty($priceTour)){
+          $maxAgencyPrice = 0;
+          foreach ($priceTour as $key => $value) {
+            # code...
+            if($value->agt_sale_adult_price > $maxAgencyPrice){
+              $result[$count]["price"] = $value;
+              $maxAgencyPrice = $value->agt_sale_adult_price;
+            }
+          }
+        }
+
+        $count++;
+      }
+      //print_r($result); exit;
+      if(!empty($result)){
+        return $result;
+      }else{
+        return false;
+      }
+
+    }else if(!empty($args["tag_id"]) && !empty($args["join"]) && !empty($args["in"])){
 
       //Get distinct tour_id from ci_tagtour
       $this->db->select('tat_tour_id');
@@ -163,6 +225,7 @@ class TagTour_model extends MY_Model {
 
         //Get tour data
         unset($this->db);
+        $this->db->select('tou_id, tou_name, tou_code, tou_url, tou_first_image, tou_banner_image');
         $this->db->where('tou_id', $value->tat_tour_id);
         $query = $this->db->get('ci_tour');
         $tourBuffer = $query->result(); 
@@ -177,9 +240,26 @@ class TagTour_model extends MY_Model {
         $query = $this->db->get('ci_tagtour');
         $result[$count]["tag"] = $query->result();
 
+
+        //Get price data
+        unset($this->db);
+        $this->db->where('agt_tour_id', $value->tat_tour_id);
+        $priceTour = $this->db->get('ci_agencytour')->result();
+
+        if(!empty($priceTour)){
+          $maxAgencyPrice = 0;
+          foreach ($priceTour as $key => $value) {
+            # code...
+            if($value->agt_sale_adult_price > $maxAgencyPrice){
+              $result[$count]["price"] = $value;
+              $maxAgencyPrice = $value->agt_sale_adult_price;
+            }
+          }
+        }
+
         $count++;
       }
-
+      //print_r($result); exit;
       if(!empty($result)){
         return $result;
       }else{
@@ -204,6 +284,7 @@ class TagTour_model extends MY_Model {
 
         //Get tour data
         unset($this->db);
+        $this->db->select('tou_id, tou_name, tou_code, tou_url, tou_first_image, tou_banner_image');
         $this->db->where('tou_id', $value->tat_tour_id);
         $query = $this->db->get('ci_tour');
         $tourBuffer = $query->result(); 
@@ -218,9 +299,25 @@ class TagTour_model extends MY_Model {
         $query = $this->db->get('ci_tagtour');
         $result[$count]["tag"] = $query->result();
 
+        //Get price data
+        unset($this->db);
+        $this->db->where('agt_tour_id', $value->tat_tour_id);
+        $priceTour = $this->db->get('ci_agencytour')->result();
+
+        if(!empty($priceTour)){
+          $maxAgencyPrice = 0;
+          foreach ($priceTour as $key => $value) {
+            # code...
+            if($value->agt_sale_adult_price > $maxAgencyPrice){
+              $result[$count]["price"] = $value;
+              $maxAgencyPrice = $value->agt_sale_adult_price;
+            }
+          }
+        }
+
         $count++;
       }
-      //print_r($result);
+      //print_r($result); exit;
 
       if(!empty($result)){
         return $result;
@@ -258,7 +355,7 @@ class TagTour_model extends MY_Model {
     }else if(!empty($args["tour_id"])){
       //Get category by name
       $data["tat_tour_id"] = $args["tour_id"];
-      //print_r($args);
+
       $query = $this->db->get_where('ci_tagtour', $data);
       if($query->num_rows > 0){
         $newResult = $this->mapField($query->result());
@@ -343,166 +440,182 @@ class TagTour_model extends MY_Model {
 
     //Get tag by tagname
 
-    if($args["tags"]=="[]"){
-      return ;
-    } 
 
-    //print_r($args["tags"]); exit;
-    $tags = str_replace('[', '', $args["tags"]);  
-    $tags = str_replace(']', '', $tags);      
-    $tags = str_replace('"', '', $tags);  
-    $tags = explode(",",  $tags);
-    $tags = array_unique($tags);
-    $this->load->model("tag_model", "tagModel");
-    $tagInput = array();
-    $count = 0;
+    if(!empty($args["tags"])){
 
+      if($args["tags"]=="[]"){
+        return ;
+      } 
 
-
-
-
-    $tagtour["tour_id"] = $args["id"];
-    $query = $this->getRecord($tagtour);
+      //print_r($args["tags"]); exit;
+      $tags = str_replace('[', '', $args["tags"]);  
+      $tags = str_replace(']', '', $tags);      
+      $tags = str_replace('"', '', $tags);  
+      $tags = explode(",",  $tags);
+      $tags = array_unique($tags);
+      $this->load->model("tag_model", "tagModel");
+      $tagInput = array();
+      $count = 0;
 
 
-    foreach ($tags as $key => $value) {
-      $tag["name"] = $value;
-      $tagQuery = $this->tagModel->getRecord($tag);
 
-      if(empty($tagQuery)){
-        $tag["url"] = Util::url_title($value);
-        $input[$count]->id = $this->tagModel->addRecord($tag);
-        $input[$count]->name = $tag["name"];
-      }else{
-        $input[$count] = $tagQuery[0];   
+
+
+      $tagtour["tour_id"] = $args["id"];
+      $query = $this->getRecord($tagtour);
+
+
+      foreach ($tags as $key => $value) {
+        $tag["name"] = $value;
+        $tagQuery = $this->tagModel->getRecord($tag);
+
+        if(empty($tagQuery)){
+          $tag["url"] = Util::url_title($value);
+          $input[$count]->id = $this->tagModel->addRecord($tag);
+          $input[$count]->name = $tag["name"];
+        }else{
+          $input[$count] = $tagQuery[0];   
+        }
+        $count++;
       }
-      $count++;
+
+
+      //print_r($input);
+      //print_r($query); exit;
+
+      $update = false;
+      $updateCount = 0;
+      $updateArray = array();
+      $updateData =  array();
+
+      $deleteCount = 0;
+      $deleteArray = array();
+
+      $insert = false;
+      $insertCount = 0;
+      $insertArray = array();
+
+      if(!empty($query)){
+        //Loop check update && delete
+        foreach ($query as $keyQuery => $valueQuery) {
+            //echo "Query : ".$valueQuery->tag_id;
+            //echo "<br><br>";
+
+            foreach ($input as $keyInput => $valueInput) {
+                //echo "Input : ".$valueInput->id;
+                //echo "<br><br>";            
+                if($valueQuery->tag_id == $valueInput->id){
+                    $update = true;
+                    $updateData = $valueInput;
+                }
+            }
+
+            if($update){
+                //Update
+                $updateArray[$updateCount] = $updateData;
+                $updateCount++;  
+                $update = false;  
+            }else{
+                //Delete
+                $deleteArray[$deleteCount] = $valueQuery;
+                $deleteCount++; 
+            }     
+        }
+
+        //Loop check insert
+        foreach ($input as $keyInput => $valueInput) {
+            //echo "Query : ".$valueInput->id;
+            //echo "<br><br>";
+            foreach ($query as $keyQuery => $valueQuery) {
+                //echo "Input : ".$valueQuery->tag_id;
+                //echo "<br><br>";            
+                if($valueInput->id == $valueQuery->tag_id){
+                    $insert = true;
+                    //$insertData = $valueInput;
+                }
+            }
+
+            //Insert
+            if($insert){
+                //$updateArray[$updateCount] = $insertData;
+                //$updateCount++;  
+                $insert = false;  
+            }else{
+                $insertArray[$insertCount] = $valueInput;
+                $insertTag[$insertCount]["tag_id"] = $valueInput->id;
+                $insertTag[$insertCount]["tour_id"] = $args["id"];
+                $insertCount++; 
+            }     
+        }
+        //return $updateArray;
+
+
+      //End check empty $query
+      }else{ 
+
+        foreach ($input as $keyInput => $valueInput) {
+          $insertArray[$insertCount] = $valueInput;
+          $insertTag[$insertCount]["tag_id"] = $valueInput->id;
+          $insertTag[$insertCount]["tour_id"] = $args["id"];
+          $insertCount++; 
+        }
+
+      }
+
+   
+
+      if(!empty($insertTag)){
+    
+        $this->addMultipleRecord($insertTag);
+      }
+
+      if(!empty($deleteArray)){
+        /*echo "<br><br>";
+        echo "Delete : ";
+        echo "<br><br>";
+        print_r($deleteArray);
+        echo "<br><br>";
+  */
+        foreach ($deleteArray as $key => $value) {
+          # code...
+          $deleteTag["id"] = $value->id;
+          $this->deleteRecord($deleteTag);
+        }
+
+      }
+      //exit;
+      /*
+        echo "Update : ";
+        echo "<br><br>";
+        print_r($updateArray);
+        echo "<br><br>";
+      */
+      //exit;
+    }else if(!empty($args["tours"])){
+      //blah blah blah
     }
-
-
-    //print_r($input);
-    //print_r($query); exit;
-
-    $update = false;
-    $updateCount = 0;
-    $updateArray = array();
-    $updateData =  array();
-
-    $deleteCount = 0;
-    $deleteArray = array();
-
-    $insert = false;
-    $insertCount = 0;
-    $insertArray = array();
-
-    if(!empty($query)){
-      //Loop check update && delete
-      foreach ($query as $keyQuery => $valueQuery) {
-          //echo "Query : ".$valueQuery->tag_id;
-          //echo "<br><br>";
-
-          foreach ($input as $keyInput => $valueInput) {
-              //echo "Input : ".$valueInput->id;
-              //echo "<br><br>";            
-              if($valueQuery->tag_id == $valueInput->id){
-                  $update = true;
-                  $updateData = $valueInput;
-              }
-          }
-
-          if($update){
-              //Update
-              $updateArray[$updateCount] = $updateData;
-              $updateCount++;  
-              $update = false;  
-          }else{
-              //Delete
-              $deleteArray[$deleteCount] = $valueQuery;
-              $deleteCount++; 
-          }     
-      }
-
-      //Loop check insert
-      foreach ($input as $keyInput => $valueInput) {
-          //echo "Query : ".$valueInput->id;
-          //echo "<br><br>";
-          foreach ($query as $keyQuery => $valueQuery) {
-              //echo "Input : ".$valueQuery->tag_id;
-              //echo "<br><br>";            
-              if($valueInput->id == $valueQuery->tag_id){
-                  $insert = true;
-                  //$insertData = $valueInput;
-              }
-          }
-
-          //Insert
-          if($insert){
-              //$updateArray[$updateCount] = $insertData;
-              //$updateCount++;  
-              $insert = false;  
-          }else{
-              $insertArray[$insertCount] = $valueInput;
-              $insertTag[$insertCount]["tag_id"] = $valueInput->id;
-              $insertTag[$insertCount]["tour_id"] = $args["id"];
-              $insertCount++; 
-          }     
-      }
-      //return $updateArray;
-
-
-    //End check empty $query
-    }else{ 
-
-      foreach ($input as $keyInput => $valueInput) {
-        $insertArray[$insertCount] = $valueInput;
-        $insertTag[$insertCount]["tag_id"] = $valueInput->id;
-        $insertTag[$insertCount]["tour_id"] = $args["id"];
-        $insertCount++; 
-      }
-
-    }
-
- 
-
-    if(!empty($insertTag)){
-  
-      $this->addMultipleRecord($insertTag);
-    }
-
-    if(!empty($deleteArray)){
-      /*echo "<br><br>";
-      echo "Delete : ";
-      echo "<br><br>";
-      print_r($deleteArray);
-      echo "<br><br>";
-*/
-      foreach ($deleteArray as $key => $value) {
-        # code...
-        $deleteTag["id"] = $value->id;
-        $this->deleteRecord($deleteTag);
-      }
-
-    }
-    //exit;
-    /*
-      echo "Update : ";
-      echo "<br><br>";
-      print_r($updateArray);
-      echo "<br><br>";
-    */
-    //exit;
         
     return ;
   }
 
   function deleteRecord($args=false){
-    if(isset($args["id"])){
+    if(!empty($args["id"])){
       $this->db->where("tat_id", $args["id"]);
       $this->db->delete("ci_tagtour");
-    }else if(isset($args["tour_id"])){
 
+    }else if(!empty($args["tag_id"]) && !empty($args["tour_id"])){
+      $this->db->where("tat_tag_id", $args["tag_id"]);
       $this->db->where("tat_tour_id", $args["tour_id"]);
       $this->db->delete("ci_tagtour");
+
+    }else if(!empty($args["tour_id"])){
+      $this->db->where("tat_tour_id", $args["tour_id"]);
+      $this->db->delete("ci_tagtour");
+
+    }else if(!empty($args["tag_id"])){
+      $this->db->where("tat_tag_id", $args["tag_id"]);
+      $this->db->delete("ci_tagtour");
+
     }
   }
 }
