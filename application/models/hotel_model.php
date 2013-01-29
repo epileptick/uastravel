e3d8642678f941227772ce87396d4782398df64d
pacawadee.s <pacawadee.s@gmail.com>
2013-01-29 02:38:38 +0000

    hotel delete

--- application/models/hotel_model.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Hotel_model extends MY_Model {
-  
-  function __construct(){
-    parent::__construct();  
-  }  
-
-}
-?>
