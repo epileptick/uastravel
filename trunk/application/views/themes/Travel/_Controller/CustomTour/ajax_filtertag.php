b52124b248e357f609096fd5dc088e027b27d22d
Ottowan@gmail.com <Ottowan@gmail.com@dd2fac05-a434-c35f-77d5-eb2919b9e563>
2013-03-08 05:47:14 +0000

    Make new hotel commit

--- trunk/application/views/themes/Travel/_Controller/CustomTour/ajax_filtertag.php
+++ trunk/application/views/themes/Travel/_Controller/CustomTour/ajax_filtertag.php
@@ -6,8 +6,8 @@
     foreach ($tours["filter"]["thirdtag"] as $key => $value) {
   ?>
       <span class="btn">
-        <?php echo $value->name;?>
-        <input type="checkbox" class="hide" name="example1[]" value="<?php echo $value->tag_id;?>">
+        <?php echo $value['name'];?>
+        <input type="checkbox" class="hide" name="example1[]" value="<?php echo $value['tag_id'];?>">
       </span>
   <?php                   
     }
