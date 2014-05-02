086a82baaa8f43c78fca84f38b7057013da4155d
ball.tuangsit@gmail.com <ball.tuangsit@gmail.com@dd2fac05-a434-c35f-77d5-eb2919b9e563>
2014-05-02 03:30:42 +0000

    update new

---
+++ trunk/application/views/themes/uastravel2014/_Controller/New/admin_index.php
@@ -0,0 +1,41 @@
+<table class="table table-hover">
+	<thead>
+		<tr>
+			<td>
+		 		ID
+			</td>
+			<td>
+		 		Title
+			</td>
+			<td>
+				Detail
+			</td>				
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				1
+			</td>
+			<td>
+				
+			</td>
+		</tr>
+		<tr>
+			<td>
+				2
+			</td>
+			<td>
+				
+			</td>
+		</tr>
+		<tr>
+			<td>
+				2
+			</td>
+			<td>
+				
+			</td>
+		</tr>		
+	</tbody>
+</table>
